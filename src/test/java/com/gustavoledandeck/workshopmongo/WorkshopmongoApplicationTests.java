c82b9f496c6372101dcb39ebd9bce89916d93cbe
Gustavo Ledandeck <guledandeck2@hotmail.com>
2024-02-13 15:24:08 -0300

    primeiro commit - projeto criado

---
+++ src/test/java/com/gustavoledandeck/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoledandeck.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
