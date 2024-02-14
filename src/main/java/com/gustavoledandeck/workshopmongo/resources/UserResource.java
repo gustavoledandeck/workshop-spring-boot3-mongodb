31b4b6bf94f3e8defc99d3eb3c90b8f2c4fb6641
Gustavo Ledandeck <guledandeck2@hotmail.com>
2024-02-14 20:24:05 -0300

    Endpoint to return user's posts

--- src/main/java/com/gustavoledandeck/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gustavoledandeck/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gustavoledandeck.workshopmongo.domain.Post;
 import com.gustavoledandeck.workshopmongo.domain.User;
 import com.gustavoledandeck.workshopmongo.dto.UserDTO;
 import com.gustavoledandeck.workshopmongo.services.UserService;
@@ -63,5 +64,10 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-	
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 }
